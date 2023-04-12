dec9e8bec6a88f227ec3a4eaa4319cddba2e4c8f
glortest <gregoryomelchuk@vk.com>
2023-04-12 16:00:03 +0300

    Added new command for developers

--- main.py
+++ main.py
@@ -7,12 +7,35 @@ from telegram.ext import CommandHandler
 
 
 
-# Запускаем логгирование
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
 )
 
 logger = logging.getLogger(__name__)
+
+TIMER = 5  # таймер на 5 секунд
+
+
+def remove_job_if_exists(name, context):
+    current_jobs = context.job_queue.get_jobs_by_name(name)
+    if not current_jobs:
+        return False
+    for job in current_jobs:
+        job.schedule_removal()
+    return True
+
+
+# Обычный обработчик, как и те, которыми мы пользовались раньше.
+async def set_timer(update, context):
+    """Добавляем задачу в очередь"""
+    chat_id = update.effective_message.chat_id
+    job_removed = remove_job_if_exists(str(chat_id), context)
+    context.job_queue.run_once(task, TIMER, chat_id=chat_id, name=str(chat_id), data=TIMER)
+
+    text = f'Вернусь через 5 с.!'
+    if job_removed:
+        text += ' Старая задача удалена.'
+    await update.effective_message.reply_text(text)
 
 
 async def readingNotifications(update, context):
@@ -71,17 +94,21 @@ def checkingAvailability(text, premium):
         return """
 Друг, ты ступил на путь истины, на путь 80ти баллов за первый год обучения, написав эту команду. 
 Для полной разблокировки бота необходимо - перевести N рублей на номер +N(NNN)NNN-NN-NN по СБП. 
+
 После оплаты в чат бота напиши такое сообщение: /+N(), поместив внутрь скобок свой номер телефона. 
-Потом придётся подожди нексколько минут, а затем наслаждаться жизнью, ведь не придётся ботать ЯЛ"""
+
+Потом подожди нексколько минут, а затем наслаждайся жизнью, ведь не придётся ботать ЯЛ"""
 
     elif text[:4] == "/+N(":
         filep = open("Request.txt", "w", encoding="utf8")
         filep.writelines(premium + " " + text[4:-1])
-        return "Теперь придётся немного подождать, уже обрабатываем вашу заявку"
+        return "Теперь придётся немного подождать, уже обрабатываем вашу заявку."
 
     elif text == "/startDebugModeLoginGlortestParol14881488":
         f = open("Request.txt", "r", encoding="utf8")
         lines = [elem.strip() + "" for elem in f.readlines()]
+        if len(lines) == 0:
+            return "Empty"
         sj = str("\n".join(lines))
         return sj
 
@@ -89,10 +116,19 @@ def checkingAvailability(text, premium):
         open('Request.txt', 'w').close()
         return "OK"
 
+    elif text == "/DebugModeLoginGlortestParol14881488ChangeAllUsers":
+        filep = open("CashUsers.txt", "w", encoding="utf8")
+        f = open("Request.txt", "r", encoding="utf8")
+        lines = [elem.strip() + "" for elem in f.readlines()]
+        if len(lines) == 0:
+            return "Empty"
+        for elem in lines:
+            print(elem.split())
+            filep.writelines(elem.split()[0])
+        return "OK"
+
     else:
         return "похоже, вы ошиблись и не правильно ввели текст"
-
-
 
 
 if __name__ == '__main__':
