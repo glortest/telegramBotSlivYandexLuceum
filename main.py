75953377be1606132fe79810e7e2ac413610c229
glortest <gregoryomelchuk@vk.com>
2023-04-18 18:10:27 +0300

    Clean project

--- main.py
+++ main.py
@@ -11,30 +11,7 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
-TIMER = 5  # таймер на 5 секунд
-
-
-def remove_job_if_exists(name, context):
-    current_jobs = context.job_queue.get_jobs_by_name(name)
-    if not current_jobs:
-        return False
-    for job in current_jobs:
-        job.schedule_removal()
-    return True
-
-
 # Обычный обработчик, как и те, которыми мы пользовались раньше.
-async def set_timer(update, context):
-    """Добавляем задачу в очередь"""
-    chat_id = update.effective_message.chat_id
-    job_removed = remove_job_if_exists(str(chat_id), context)
-    context.job_queue.run_once(task, TIMER, chat_id=chat_id, name=str(chat_id), data=TIMER)
-
-    text = f'Вернусь через 5 с.!'
-    if job_removed:
-        text += ' Старая задача удалена.'
-    await update.effective_message.reply_text(text)
-
 
 async def readingNotifications(update, context):
     print('id: ' + str(update.message.from_user.id) + '; username: ' +
